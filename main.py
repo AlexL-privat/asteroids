e90fcf76525d85f0f43c67211adf143c91455998
AlexL-privat <alexander.lauenroth@gmail.com>
2025-10-20 18:49:59 +0200

    first step

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0
 
 def main():
     print("Starting Asteroids!")
@@ -18,7 +20,8 @@ def main():
             if event.type == pygame.QUIT:
              return
         screen.fill(000000)
-        pygame.display.flip() 
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
